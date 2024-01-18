b010743254424875043812a1745f6d274ac2794b
JonatanDrysen <jojodrysen@gmail.com>
2024-01-18 14:40:00 +0100

    add temp converter fn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-function checkTemp(tempF) {
+document.getElementById("tempForm").addEventListener("submit", function checkTemp(event) {
+    event.preventDefault()
+    const tempF = event.target.elements[0].value
+    const tempC = convertFahrenheit(tempF)
+})
 
+function convertFahrenheit(tempF) {
+    const tempC = Math.floor((tempF - 32) / 1.8)
+    return tempC
 }
